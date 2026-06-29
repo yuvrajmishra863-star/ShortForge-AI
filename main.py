3d1120471869703f4e4f459b4cec2abab7d39c01
Yuvraj Mishra <your-email@example.com>
2026-06-29 23:47:15 +0530

    Added loading animation and character counter

--- main.py
+++ main.py
@@ -1,70 +1,36 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from app.ai import generate_script
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <html>
-    <head>
-        <title>ShortForge AI</title>
-    </head>
-    <body style="font-family:Arial;text-align:center;margin-top:60px;">
-
-        <h1>🎬 ShortForge AI</h1>
-
-        <form action="/generate" method="post">
-
-            <input
-                type="text"
-                name="prompt"
-                placeholder="Enter your video topic"
-                style="width:350px;height:35px;"
-            >
-
-            <br><br>
-
-            <button type="submit">
-                Generate AI Script
-            </button>
-
-        </form>
-
-    </body>
-    </html>
-    """
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
 
 
 @app.post("/generate", response_class=HTMLResponse)
-def generate(prompt: str = Form(...)):
+async def generate(request: Request, prompt: str = Form(...)):
 
     script = generate_script(prompt)
 
-    return f"""
-    <html>
-
-    <body style="font-family:Arial;margin:40px;">
-
-        <h1>🎬 AI Generated Script</h1>
-
-        <h3>Topic:</h3>
-
-        <p>{prompt}</p>
-
-        <hr>
-
-        <pre style="white-space:pre-wrap;font-size:18px;">
-{script}
-        </pre>
-
-        <br>
-
-        <a href="/">⬅ Generate Another</a>
-
-    </body>
-
-    </html>
-    """
+    return templates.TemplateResponse(
+        request=request,
+        name="result.html",
+        context={
+            "prompt": prompt,
+            "script": script
+        }
+    )
