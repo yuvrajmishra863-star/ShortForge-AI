21c7afb4311c01a2c8db48f155b35e527f4cb080
Yuvraj Mishra <your-email@example.com>
2026-06-30 12:57:00 +0530

    Working AI video generator

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/js/**/*.js",
+    "./app/**/*.py",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
